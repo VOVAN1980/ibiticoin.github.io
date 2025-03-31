9e8de8ec134a8dd0c36837037f0b30a7d164092a
VOVAN1980 <reker198079@gmail.com>
2025-03-31 23:39:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// config.js
-
 const config = {
   testnet: {
     networkName: "BSC Testnet",
@@ -9,15 +7,12 @@ const config = {
     contracts: {
       IBITI_TOKEN_ADDRESS: "0xcADDCB8541f222b3Cd635e5789700Befc4437Bed",  // IBITIcoin
       FEE_MANAGER_ADDRESS: "0x358a8c03ECbe659B42BF88Ab6B9853971dA0A9dd",   // FeeManager
-      NFTDiscount_ADDRESS: "0x58A2e9a323319EA9056b5826f83f2caC202C6917",     // NFTDiscount
+      NFTDiscount_ADDRESS: "0x58A2e9a323319EA9056b5826f83f2caC202C6917",    // NFTDiscount
+      IBITI_NFT_ADDRESS: "0x35b65240Cb6735Fc0458c4563f43F0C4464890A6",        // IBITINFT
+      STAKING_MODULE_ADDRESS: "0x295fd16d48027CCb1e089ddFF09f7983200833F7",     // StakingModule
+      DAO_MODULE_ADDRESS: "0x4AFd9c948e99Ce085e9d92f01987FF4d2d6D8c00",          // DAOModuleImplementation
       USER_STATUS_MANAGER_ADDRESS: "0xEe2102d4d0C68B7Db735456eccaE8D4A315Bf1A0", // UserStatusManager
-      ERC20_MOCK_ADDRESS: "0x255aB3eC7Fb75cE360F0AC4392d2F5825B6F87FD",       // ERC20Mock
-      IBITI_NFT_ADDRESS: "0x35b65240Cb6735Fc0458c4563f43F0C4464890A6",         // IBITINFT
-      IBITI_STAKING_ADDRESS: "0x295fd16d48027CCb1e089ddFF09f7983200833F7",      // StakingModule
-      DAOMODULE_ADDRESS: "0x4AFd9c948e99Ce085e9d92f01987FF4d2d6D8c00",         // DAOModuleImplementation
-      BRIDGE_MANAGER_ADDRESS: "0xa7B2D69b993C1c0b18bd817DF80C510BD5da383C",      // BridgeManager
-      IBITI_NFT_IMPL_ADDRESS: "",      // IBITINFT (используем тот же адрес)
-      TEAM_VESTING_ADDRESS: "0x7a72825B92b31328e099186F50180BA4295ebc8b"         // TeamVesting
+      BRIDGE_MANAGER_ADDRESS: "0xa7B2D69b993C1c0b18bd817DF80C510BD5da383C"       // BridgeManager
     }
   },
   mainnet: {
@@ -29,17 +24,13 @@ const config = {
       IBITI_TOKEN_ADDRESS: "0x...", // заполните после деплоя на Mainnet
       FEE_MANAGER_ADDRESS: "0x...",
       NFTDiscount_ADDRESS: "0x...",
-      IBITINFT_ADDRESS: "0x...",
-      IBITI_STAKING_ADDRESS: "0x...",
-      DAOMODULE_ADDRESS: "0x...",
+      IBITI_NFT_ADDRESS: "0x...",
+      STAKING_MODULE_ADDRESS: "0x...",
+      DAO_MODULE_ADDRESS: "0x...",
       USER_STATUS_MANAGER_ADDRESS: "0x...",
-      BRIDGE_MANAGER_ADDRESS: "0x...",
-      ERC20_MOCK_ADDRESS: "0x...",
-      IBITI_NFT_IMPL_ADDRESS: "0x...",
-      TEAM_VESTING_ADDRESS: "0x..."
+      BRIDGE_MANAGER_ADDRESS: "0x..."
     }
   }
 };
 
-// Если переменная process.env.NODE_ENV не определена, можно выбрать нужный раздел, например "testnet".
-export default config["testnet"];
+export default config.testnet;
