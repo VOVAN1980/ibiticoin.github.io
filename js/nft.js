b42a035411ef4b3ed1348c98e5d1a18618b36147
VOVAN1980 <reker198079@gmail.com>
2025-04-01 03:40:04 +0300

    Update nft.js

--- js/nft.js
+++ js/nft.js
@@ -1,12 +1,12 @@
 import { ethers } from "ethers";
 import config from "../config.js"; // Убедитесь, что путь корректный
-import nftAbi from "./abis/IBITINFT.json"; // ABI контракта IBITINFT должен быть в этой папке
+import nftAbi from "./abis/IBITINFT.json"; // ABI контракта IBITINFT
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
 const nftContract = new ethers.Contract(
-  config.contracts.IBITI_NFT_ADDRESS,
+  config.contracts.IBITI_NFT_ADDRESS, // адрес подтягивается из конфигурации
   nftAbi,
   signer
 );
