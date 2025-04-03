fb601c4e2925589a78c351fd7bd50aa67ec48228
VOVAN1980 <reker198079@gmail.com>
2025-04-03 04:48:22 +0300

    Add files via upload

---
+++ js/abis/IBITIcoin.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "symbol",
+        "type": "string"
+      },
+      {
+        "internalType": "address",
+        "name": "_founderWallet",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_reserveWallet",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_teamVesting",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_feeManager",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_stakingModule",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_daoModule",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_userStatusManager",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_bridgeManager",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_nftContract",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "InvalidShortString",
+    "type": "error"
+  }
+];
