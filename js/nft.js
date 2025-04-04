03685fef656a9317d9738984e8dc421f9878ab48
VOVAN1980 <reker198079@gmail.com>
2025-04-04 23:09:24 +0300

    Update nft.js
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- js/nft.js
+++ js/nft.js
@@ -3,20 +3,22 @@ import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.es
 import config from "../config.js";
 import { ibitiNftAbi } from "./abis/IBITINFT.js";
 
-// Проверка MetaMask
+// Определяем активную сеть (по config)
+const netConfig = config.testnet ?? config;
+
 if (!window.ethereum) {
+  alert("MetaMask не установлен. Установите расширение для работы с NFT.");
   console.warn("MetaMask не обнаружен. NFT-модуль не активен.");
 } else {
   const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   const signer = provider.getSigner();
 
   const nftContract = new ethers.Contract(
-    config.testnet.contracts.IBITI_NFT_ADDRESS,
+    netConfig.contracts.IBITI_NFT_ADDRESS,
     ibitiNftAbi,
     signer
   );
 
-  // Получение баланса NFT
   async function getNFTBalance() {
     try {
       const address = await signer.getAddress();
@@ -27,10 +29,9 @@ if (!window.ethereum) {
     }
   }
 
-  // При загрузке
   getNFTBalance();
 
-  // Глобальный экспорт (для кнопок, отладки и т.д.)
+  // Глобальный экспорт
   window.nftContract = nftContract;
   window.getNFTBalance = getNFTBalance;
 }
