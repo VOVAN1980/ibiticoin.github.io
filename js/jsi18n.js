f19c89b7059bb09f21255b219100d3bc2e676b78
VOVAN1980 <reker198079@gmail.com>
2025-05-26 00:37:07 +0300

    Update jsi18n.js
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- js/jsi18n.js
+++ js/jsi18n.js
@@ -1,13 +1,26 @@
-i18next
-  .use(i18nextHttpBackend)
-  .use(i18nextBrowserLanguageDetector)
-  .init({
-    fallbackLng: 'ru',
-    debug: true,
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json'
+import i18next from 'https://unpkg.com/i18next@23.4.6/dist/esm/i18next.js';
+
+await i18next.init({
+  lng: 'ru', // язык по умолчанию
+  fallbackLng: 'ru',
+  resources: {
+    en: {
+      translation: {
+        "staking_title": "IBITI Staking",
+        "staking_description": "Stake tokens and earn rewards",
+        "connect_wallet": "Connect Wallet",
+        "home": "Home",
+        "share": "Share"
+      }
+    },
+    ru: {
+      translation: {
+        "staking_title": "Стейкинг IBITI",
+        "staking_description": "Застейкай токены и получай награды",
+        "connect_wallet": "Подключить кошелёк",
+        "home": "Главная",
+        "share": "Поделиться"
+      }
     }
-  }, function(err, t) {
-    jqueryI18next.init(i18next, $, { useOptionsAttr: true });
-    $('body').localize();
-  });
+  }
+});
