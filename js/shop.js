3ea26226ce1b2017900d2373bb4ba9ae19b45590
VOVAN1980 <reker198079@gmail.com>
2025-06-11 04:50:38 +0300

    Update shop.js
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- js/shop.js
+++ js/shop.js
@@ -9,6 +9,8 @@ import { PhasedTokenSaleAbi }                    from "./abis/PhasedTokenSaleAbi
 import { ibitiTokenAbi }                         from "./abis/ibitiTokenAbi.js";
 
 // Публичный RPC и контракт для чтения данных по IBITI
+const rpcProvider = new ethers.JsonRpcProvider(config.active.rpcUrl);
+
 const readSaleContract = new ethers.Contract(
   config.active.contracts.PHASED_TOKENSALE,
   PhasedTokenSaleAbi,
