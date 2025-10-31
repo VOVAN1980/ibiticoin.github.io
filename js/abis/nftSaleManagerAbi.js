33b3ad3d0e9d42c1165f49fe2bfb0833fe6bed40
VOVAN1980 <reker198079@gmail.com>
2025-10-31 20:13:52 +0100

    Update nftSaleManagerAbi.js
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- js/abis/nftSaleManagerAbi.js
+++ js/abis/nftSaleManagerAbi.js
@@ -1,476 +1,476 @@
 export const nftSaleManagerAbi = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "_nftDiscount",
-        type: "address"
+        "internalType": "address",
+        "name": "_nftDiscount",
+        "type": "address"
       },
       {
-        internalType: "address",
-        name: "_ibitiToken",
-        type: "address"
+        "internalType": "address",
+        "name": "_ibitiToken",
+        "type": "address"
       },
       {
-        internalType: "address",
-        name: "_usdtToken",
-        type: "address"
+        "internalType": "address",
+        "name": "_usdtToken",
+        "type": "address"
       },
       {
-        internalType: "address",
-        name: "_priceOracle",
-        type: "address"
+        "internalType": "address",
+        "name": "_priceOracle",
+        "type": "address"
       }
     ],
-    stateMutability: "nonpayable",
-    type: "constructor"
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "buyer",
-        type: "address"
+        "indexed": true,
+        "internalType": "address",
+        "name": "buyer",
+        "type": "address"
       },
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       },
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "paidAmount",
-        type: "uint256"
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "paidAmount",
+        "type": "uint256"
       },
       {
-        indexed: false,
-        internalType: "address",
-        name: "paymentToken",
-        type: "address"
+        "indexed": false,
+        "internalType": "address",
+        "name": "paymentToken",
+        "type": "address"
       }
     ],
-    name: "NFTPurchased",
-    type: "event"
+    "name": "NFTPurchased",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "bool",
-        name: "enabled",
-        type: "bool"
+        "indexed": false,
+        "internalType": "bool",
+        "name": "enabled",
+        "type": "bool"
       }
     ],
-    name: "OracleToggled",
-    type: "event"
+    "name": "OracleToggled",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "address",
-        name: "newOracle",
-        type: "address"
+        "indexed": false,
+        "internalType": "address",
+        "name": "newOracle",
+        "type": "address"
       }
     ],
-    name: "OracleUpdated",
-    type: "event"
+    "name": "OracleUpdated",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address"
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
       },
       {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address"
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
       }
     ],
-    name: "OwnershipTransferred",
-    type: "event"
+    "name": "OwnershipTransferred",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "priceUSD",
-        type: "uint256"
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "priceUSD",
+        "type": "uint256"
       }
     ],
-    name: "PandoraPriceSet",
-    type: "event"
+    "name": "PandoraPriceSet",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "address",
-        name: "account",
-        type: "address"
+        "indexed": false,
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
       }
     ],
-    name: "Paused",
-    type: "event"
+    "name": "Paused",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       },
       {
-        indexed: false,
-        internalType: "uint256",
-        name: "priceUSD",
-        type: "uint256"
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "priceUSD",
+        "type": "uint256"
       }
     ],
-    name: "PriceSet",
-    type: "event"
+    "name": "PriceSet",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: false,
-        internalType: "address",
-        name: "account",
-        type: "address"
+        "indexed": false,
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
       }
     ],
-    name: "Unpaused",
-    type: "event"
+    "name": "Unpaused",
+    "type": "event"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       },
       {
-        internalType: "string",
-        name: "uri",
-        type: "string"
+        "internalType": "string",
+        "name": "uri",
+        "type": "string"
       }
     ],
-    name: "buyNFTWithIBITI",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "buyNFTWithIBITI",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       },
       {
-        internalType: "string",
-        name: "uri",
-        type: "string"
+        "internalType": "string",
+        "name": "uri",
+        "type": "string"
       }
     ],
-    name: "buyNFTWithUSDT",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "buyNFTWithUSDT",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "uri",
-        type: "string"
+        "internalType": "string",
+        "name": "uri",
+        "type": "string"
       }
     ],
-    name: "buyPandoraWithIBITI",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "buyPandoraWithIBITI",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "uri",
-        type: "string"
+        "internalType": "string",
+        "name": "uri",
+        "type": "string"
       }
     ],
-    name: "buyPandoraWithUSDT",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "buyPandoraWithUSDT",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       }
     ],
-    name: "getCurrentIBITIPrice",
-    outputs: [
+    "name": "getCurrentIBITIPrice",
+    "outputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       }
     ],
-    name: "getCurrentUSDTPrice",
-    outputs: [
+    "name": "getCurrentUSDTPrice",
+    "outputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "ibitiToken",
-    outputs: [
+    "inputs": [],
+    "name": "ibitiToken",
+    "outputs": [
       {
-        internalType: "contract IERC20Metadata",
-        name: "",
-        type: "address"
+        "internalType": "contract IERC20Metadata",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "nftDiscount",
-    outputs: [
+    "inputs": [],
+    "name": "nftDiscount",
+    "outputs": [
       {
-        internalType: "contract NFTDiscount",
-        name: "",
-        type: "address"
+        "internalType": "contract NFTDiscount",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    name: "nftPriceUSD",
-    outputs: [
+    "name": "nftPriceUSD",
+    "outputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "oracleEnabled",
-    outputs: [
+    "inputs": [],
+    "name": "oracleEnabled",
+    "outputs": [
       {
-        internalType: "bool",
-        name: "",
-        type: "bool"
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "owner",
-    outputs: [
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address"
+        "internalType": "address",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "pandoraPriceUSD",
-    outputs: [
+    "inputs": [],
+    "name": "pandoraPriceUSD",
+    "outputs": [
       {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "pause",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "inputs": [],
+    "name": "pause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "paused",
-    outputs: [
+    "inputs": [],
+    "name": "paused",
+    "outputs": [
       {
-        internalType: "bool",
-        name: "",
-        type: "bool"
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "priceOracle",
-    outputs: [
+    "inputs": [],
+    "name": "priceOracle",
+    "outputs": [
       {
-        internalType: "contract VolumeWeightedOracle",
-        name: "",
-        type: "address"
+        "internalType": "contract VolumeWeightedOracle",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "discountPercent",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "discountPercent",
+        "type": "uint256"
       },
       {
-        internalType: "uint256",
-        name: "priceUSD",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "priceUSD",
+        "type": "uint256"
       }
     ],
-    name: "setNFTPrice",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "setNFTPrice",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bool",
-        name: "_on",
-        type: "bool"
+        "internalType": "bool",
+        "name": "_on",
+        "type": "bool"
       }
     ],
-    name: "setOracleEnabled",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "setOracleEnabled",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "priceUSD",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "priceUSD",
+        "type": "uint256"
       }
     ],
-    name: "setPandoraPrice",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "setPandoraPrice",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "newOwner",
-        type: "address"
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
       }
     ],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "unpause",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "inputs": [],
+    "name": "unpause",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "newOracle",
-        type: "address"
+        "internalType": "address",
+        "name": "newOracle",
+        "type": "address"
       }
     ],
-    name: "updateOracle",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "updateOracle",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "usdtToken",
-    outputs: [
+    "inputs": [],
+    "name": "usdtToken",
+    "outputs": [
       {
-        internalType: "contract IERC20Metadata",
-        name: "",
-        type: "address"
+        "internalType": "contract IERC20Metadata",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: "view",
-    type: "function"
+    "stateMutability": "view",
+    "type": "function"
   }
 ]
