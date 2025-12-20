1681c005e7c465ef8d4496d5f2f7644b663c66c8
VOVAN1980 <reker198079@gmail.com>
2025-12-20 01:12:04 +0100

    Update config.js
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,26 +1,15 @@
-const config = {
-  testnet: {
-    networkName: "BSC Testnet",
-    chainId: 97,
-    rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-    explorerUrl: "https://testnet.bscscan.com",
-    contracts: {
-      // из .env
-      PANCAKESWAP_ROUTER_ADDRESS=0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3
-      USDT_TOKEN_ADDRESS_TESTNET=0x3ECc84DaEc5cA5973398C8AcA6FE5F40558aec84
-      IBITI_TOKEN_ADDRESS=0xc230f9394875305ac83013C0186a400865bc8f86
-      REFERRAL_SWAP_ROUTER:"0xF0cBFFC1a3F761771D2270d93A76124b4d1F1124", // REFERRAL_SWAP_ROUTER_ADDRESS_TESTNET
-    }
+testnet: {
+  networkName: "BSC Testnet",
+  chainId: 97,
+  rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  explorerUrl: "https://testnet.bscscan.com",
+  contracts: {
+    USDT_TOKEN:          "0x1133CfB59D870B927ca204bc5567b48B448329Fc", // твой mUSDT18
+    IBITI_TOKEN:         "0xc230f9394875305ac83013C0186a400865bc8f86",
+    PANCAKESWAP_ROUTER:  "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3",
+
+    REFERRAL_SWAP_ROUTER: "0xF0cBFFC1a3F761771D2270d93A76124b4d1F1124",
   }
-};
-
-// сейчас тестим только testnet
+},
+// и внизу
 config.active = config.testnet;
-
-export default config;
-
-
-
-
-
-
